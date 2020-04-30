dc8aece342420cf03e7b200a312e385dbb40c926
Edinson Nuñez More <nmedinson@gmail.com>
2020-04-30 11:18:56 -0500

    add spyOn

--- winners.test.js
+++ winners.test.js
@@ -1,16 +1,12 @@
 const { thumbWar } = require("./winners");
 const Utils = require("./utils");
 
-function fn(impl) {
-  const mockFn = (...args) => {
-    return impl(...args);
-  };
-  return mockFn;
-}
 test("return winner", () => {
-  const originalGetWinner = Utils.getWinner;
-  Utils.getWinner = jest.fn((p1, p2) => p1);
+  jest.spyOn(Utils, "getWinner");
+  Utils.getWinner.mockImplementation((p1, p2) => p1);
   const winner = thumbWar("Renato Echavarria", "Gustavo Garcia");
   expect(winner).toBe("Renato Echavarria");
   expect(Utils.getWinner).toHaveBeenCalled();
+  expect(Utils.getWinner).toHaveBeenCalledTimes(2);
+  Utils.getWinner.mockRestore();
 });
